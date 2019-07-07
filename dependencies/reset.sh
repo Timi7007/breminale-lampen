f5aba8208256639c915b63b4caa7ba0896b6a016
Timi7007 <timi7007@googlemail.com>
2019-07-07 18:52:22 +0200

    IP as var

--- dependencies/reset.sh
+++ dependencies/reset.sh
@@ -1,1 +1,3 @@
-curl -s 'http://192.168.2.192/set?bottom=0&middle=0&top=0' && sleep 0.2 && curl -s 'http://192.168.2.192/set?bottom=1&middle=0&top=0' && sleep 0.2 && curl -s 'http://192.168.2.192/set?bottom=1&middle=1&top=0' && sleep 0.2 && curl -s 'http://192.168.2.192/set?bottom=1&middle=1&top=1'
+lampenip='192.168.2.143'
+
+curl -s 'http://$lampenip/set?bottom=0&middle=0&top=0' && sleep 0.2 && curl -s 'http://$lampenip/set?bottom=1&middle=0&top=0' && sleep 0.2 && curl -s 'http://$lampenip/set?bottom=1&middle=1&top=0' && sleep 0.2 && curl -s 'http://$lampenip/set?bottom=1&middle=1&top=1'
